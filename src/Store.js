39a0667ddd63dae8f10ffebfb5964ccfa59f89ae
Bina <benjaminbinyam500@gmail.com>
2025-03-09 16:01:26 +0300

    Redux dev-tools

--- src/Store.js
+++ src/Store.js
@@ -1,15 +1,18 @@
 import { combineReducers, createStore } from "redux";
-import {thunk} from "redux-thunk"
+import { thunk } from "redux-thunk";
 import { applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
-
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
