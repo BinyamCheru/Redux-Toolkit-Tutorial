7cc274984e2220418d1e5bd4dfbfac456cb4056c
Bina <benjaminbinyam500@gmail.com>
2025-03-11 05:51:12 +0300

    Install redux toolkit and remove classic setup of store

--- src/Store.js
+++ src/Store.js
@@ -1,18 +1,27 @@
-import { combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { combineReducers, createStore } from "redux";
+// import { thunk } from "redux-thunk";
+// import { applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
 
 export default store;
